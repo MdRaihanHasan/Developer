a8b41a2c6b556f82f4968cdf5eb6856f967b6dfd
Raihan <itsrahul880@gmail.com>
2024-05-23 15:18:24 +0600

    update

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -1,4 +1,74 @@
-<h1>
-    dasa
-dss
-</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Developer Information</title>
+    
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+        background-color: #f4f4f4;
+    }
+
+    .section-container {
+        max-width: 800px;
+        margin: 87px auto 125px auto;
+        padding: 20px;
+        background-color: #fff;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        font-weight: 500;
+    }
+
+    .section-header {
+        margin-bottom: 10px;
+        color: #333;
+    }
+
+    .section-paragraph {
+        margin-bottom: 20px;
+        line-height: 1.6;
+        color: #555;
+    }
+
+    .contact-info {
+        margin-top: 20px;
+    }
+
+    .contact-info p {
+        margin: 5px 0;
+    }
+
+    .section-paragraph a {
+        color: #007bff;
+        text-decoration: none;
+    }
+
+    .section-paragraph a:hover {
+        text-decoration: underline;
+    }
+    .get-started {
+        display: none;
+    }
+</style>
+</head>
+<body>
+    <div class="less-space">
+        <section class="content">
+            <div class="section-container">
+                <h2 class="section-header">Developer Information</h2>
+                <p class="section-paragraph">This website is designed and developed by <a href="https://www.makehub.com.bd/#contact" rel="nofollow">Make Hub</a>.</p>
+                <p class="section-paragraph">For any web design and development query please contact</p>
+                <div class="contact-info">
+                    <p class="section-paragraph"><a class="" href="https://www.makehub.com.bd/#contact">https://www.makehub.com.bd/#contact</a></p>
+                    <p class="section-paragraph">Email: <a class="" href="mailto:support@makehub.com.bd">support@makehub.com.bd</a>, <a class="" href="mailto:admin@makehub.com.bd">admin@makehub.com.bd</a></p>
+                    <p class="section-paragraph">Contact: <a class="" href="tel:+8801722320330">+8801722320330</a>, <a class="" href="tel:+8801717821980">+8801717821980</a></p>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
